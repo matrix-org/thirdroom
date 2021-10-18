8f20d16c3f8ad80c0bc3d5a9b1fac8144c656a4d
Anthony Burchell <itartist.pdf@gmail.com>
2021-10-17 23:21:17 -0500

    removes type declarations and updates three-omi package

--- packages/core/src/@types/three-omi/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "three-omi" {
-  import { GLTFParser } from "three/examples/jsm/loaders/GLTFLoader";
-  import { AudioListener } from "three";
-
-  export class GLTFAudioEmitterExtension {
-    constructor(parse: GLTFParser, listener: AudioListener);
-  }
-}
