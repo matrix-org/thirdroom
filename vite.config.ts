f37438313e1c26147f07728c8a4bd759d3d7ae73
ajbura <ajbura@gmail.com>
2022-02-10 17:00:44 +0530

    Add postcss-preset-env
    Signed-off-by: ajbura <ajbura@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import postcssPresetEnv from 'postcss-preset-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        postcssPresetEnv({
+          stage: 3,
+          browsers: 'last 2 versions',
+          features: {
+            'nesting-rules': true,
+            'custom-media-queries': true,
+            'custom-selectors': true,
+            'custom-properties': false,
+          },
+          autoprefixer: true,
+        }),
+      ],
+    }
+  }
 })
