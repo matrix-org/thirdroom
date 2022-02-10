ab5b57ba4323538d2fa00549ffa7bf6a48cb52a9
ajbura <ajbura@gmail.com>
2022-02-10 11:13:18 +0530

    Fix bug in useVMProps hook
    Signed-off-by: ajbura <ajbura@gmail.com>

--- src/ui/hooks/useVMProp.ts
+++ src/ui/hooks/useVMProp.ts
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useLayoutEffect } from 'react';
 
 import { ViewModel } from 'hydrogen-view-sdk';
 
 export function useVMProp(vm: typeof ViewModel, propName: string) {
   const [prop, setProp] = useState(vm[propName]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const dispose = vm.disposableOn('change', (changedProp: string) => {
       if (changedProp !== propName) return;
       // TODO: check if prop has changed or not.
-      setProp(changedProp);
+      setProp(vm[propName]);
     });
     return () => dispose();
   }, [vm, propName]);
