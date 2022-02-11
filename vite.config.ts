4302cded46986085444e8f1000ee0c264b5c69df
ajbura <ajbura@gmail.com>
2022-02-11 12:25:37 +0530

    Add postcss plugins
    Signed-off-by: ajbura <ajbura@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import postcssImport from 'postcss-import';
 import postcssPresetEnv from 'postcss-preset-env';
+import postcssMixin from 'postcss-mixins';
+import postcssExtend from 'postcss-extend';
+import postcssUtilities from 'postcss-utilities';
+import postcssNested from 'postcss-nested';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,14 +13,18 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
+        postcssImport(),
+        postcssUtilities({ centerMethod: 'flexbox' }),
+        postcssMixin(),
+        // postcssExtend(), FIXME: Not working in nested rules
+        postcssNested(),
         postcssPresetEnv({
           stage: 3,
           browsers: 'last 2 versions',
           features: {
-            'nesting-rules': true,
             'custom-media-queries': true,
             'custom-selectors': true,
-            'custom-properties': false,
+            'custom-properties': true,
           },
           autoprefixer: true,
         }),
