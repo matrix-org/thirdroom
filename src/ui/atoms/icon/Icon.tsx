94776e29b70eba9c77c8976ff67156389adf80c0
ajbura <ajbura@gmail.com>
2022-02-14 09:55:28 +0530

    Fix directly applying icon color
    Signed-off-by: ajbura <ajbura@gmail.com>

--- src/ui/atoms/icon/Icon.tsx
+++ src/ui/atoms/icon/Icon.tsx
@@ -2,20 +2,20 @@ import React, { CSSProperties } from 'react';
 import './Icon.css';
 
 interface IIcon {
-  color?: string,
+  color?: string | undefined,
   size?: 'normal' | 'small' | 'extra-small',
   src: string,
   isImage?: boolean
 }
 
 export function Icon({
-  color = 'var(--ic-surface-normal)',
+  color = undefined,
   size = 'normal',
   src,
   isImage = false,
 }: IIcon) {
   const style: CSSProperties = {};
-  if (color !== null) style.backgroundColor = color;
+  if (typeof color === 'string') style.backgroundColor = color;
   if (isImage) {
     style.backgroundColor = 'transparent';
     style.backgroundImage = `url(${src})`;
