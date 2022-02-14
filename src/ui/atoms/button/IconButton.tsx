2dd766b2cd1d5026c9a9d944373ef68fd31b2b86
ajbura <ajbura@gmail.com>
2022-02-14 14:38:58 +0530

    Add icon button component
    Signed-off-by: ajbura <ajbura@gmail.com>

---
+++ src/ui/atoms/button/IconButton.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './IconButton.css';
+
+import { Icon } from '../icon/Icon';
+import { RawButton } from './RawButton';
+
+interface IIconButton {
+  className?: string,
+  variant?: 'surface' | 'primary' | 'secondary' | 'positive' | 'danger',
+  size?: 'normal' | 'small',
+  isCircle?: boolean,
+  shadedSurface?: boolean,
+  iconSrc: string,
+  type?: 'button' | 'submit' | 'reset',
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+  disabled?: boolean,
+}
+
+export function IconButton({
+  className = undefined,
+  variant = 'surface',
+  size = 'normal',
+  isCircle = false,
+  shadedSurface = false,
+  iconSrc,
+  type = 'button',
+  onClick,
+  disabled = false,
+}: IIconButton) {
+  const classes: string[] = [];
+  if (className) classes.push(className);
+  classes.push(`IconButton IconButton--${size}`);
+  if (isCircle) classes.push('IconButton--circle');
+  if (shadedSurface && variant === "surface") classes.push('IconButton--shaded');
+  
+  return (
+    <RawButton
+      className={classes.join(' ')}
+      variant={variant}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <Icon size={size} src={iconSrc} />
+    </RawButton>
+  );
+}
