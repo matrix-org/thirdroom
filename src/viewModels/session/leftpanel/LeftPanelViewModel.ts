f070363f2110c58cfbb7f19e1602e8a4fb25c6bb
ajbura <ajbura@gmail.com>
2022-02-16 18:14:58 +0530

    Add left panel view model
    Signed-off-by: ajbura <ajbura@gmail.com>

---
+++ src/viewModels/session/leftpanel/LeftPanelViewModel.ts
@@ -0,0 +1,39 @@
+import {
+  Client,
+  ViewModel,
+} from 'hydrogen-view-sdk';
+
+import { SidebarViewModel } from './SidebarViewModel';
+import { RoomListViewModel } from './RoomListViewModal';
+
+export class LeftPanelViewModel extends ViewModel {
+  private _client: typeof Client;
+  private _sidebarViewModel: SidebarViewModel;
+  private _roomListViewModel: RoomListViewModel;
+  
+  constructor(options) {
+    super(options);
+    this._client = options.client;
+    this._session = options.session;
+
+    this._sidebarViewModel = new SidebarViewModel(this.childOptions({
+      // TODO: pass user profile for now
+    }));
+    this.track(this._sidebarViewModel);
+
+    this._roomListViewModel = new RoomListViewModel(this.childOptions({
+      session: this._session,
+      invites: this._client.session.invites,
+      rooms: this._client.session.rooms,
+    }));
+    this.track(this._roomListViewModel);
+  }
+
+  get sidebarViewModel() {
+    return this._sidebarViewModel;
+  }
+
+  get roomListViewModel() {
+    return this._roomListViewModel;
+  }
+}
