b25c3ce6f05b08357f55463470e98a3145dedd34
Ajay Bura <ajbura@gmail.com>
2022-03-01 15:02:57 +0530

    Add room view contols
    Signed-off-by: Ajay Bura <ajbura@gmail.com>

--- src/ui/views/session/room/RoomView.tsx
+++ src/ui/views/session/room/RoomView.tsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React from 'react';
 import './RoomView.css';
 import { RoomViewModel } from '../../../../viewModels/session/room/RoomViewModel';
 
+import { Button } from '../../../atoms/button/Button';
+import { IconButton } from '../../../atoms/button/IconButton';
 import { ChatView } from './ChatView';
+
+import GridIC from '../../../../../res/ic/grid.svg';
+import MicIC from '../../../../../res/ic/mic.svg';
+import MessageIC from '../../../../../res/ic/message.svg';
+import SettingIC from '../../../../../res/ic/setting.svg';
+import LogoutIC from '../../../../../res/ic/logout.svg';
 
 interface IRoomView {
   vm: RoomViewModel,
   roomId: string,
+}
+
+export function RoomFloatingView({
+  vm,
+  roomId,
+}: IRoomView) {
+  // TODO: move this state to RoomViewModel
+  const [chatVisibility, setChatVisibility] = React.useState(false);
+
+  return (
+    <>
+      <Button size="small" iconSrc={GridIC} onClick={() => alert('open overlay')}>Open Overlay [Esc]</Button>
+      <div className={`RoomView__chat${chatVisibility ? ' RoomView__chat--visible' : ''}`}>
+        <ChatView roomId={roomId} vm={vm.chatViewModel} />
+      </div>
+      <div className="RoomView__controls flex">
+        <IconButton shadedSurface={true} label="Mic" size="small" iconSrc={MicIC} onClick={() => alert('mic')}/>
+        <IconButton
+          variant={chatVisibility ? 'secondary' : 'surface'}
+          shadedSurface={chatVisibility ? false : true}
+          label="Message"
+          size="small"
+          iconSrc={MessageIC}
+          onClick={() => setChatVisibility(!chatVisibility)}
+        />
+        <IconButton shadedSurface={true} label="Settings" size="small" iconSrc={SettingIC} onClick={() => alert('Settings')}/>
+        <IconButton variant="danger" label="Logout" size="small" iconSrc={LogoutIC} onClick={() => alert('Logout')}/>
+      </div>
+    </>
+  );
 }
 
 export function RoomView({
@@ -14,8 +52,9 @@ export function RoomView({
   roomId,
 }: IRoomView) {
   return (
-    <div className="RoomView grow">
-      <ChatView roomId={roomId} vm={vm.chatViewModel} />
+    <div className="RoomView grow flex">
+      <div className="RoomView__3d grow" />
+      <RoomFloatingView vm={vm} roomId={roomId} />
     </div>
   );
 }
