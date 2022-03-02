ae415d44d6c3e143fdb4a06a2b3d1030602dea84
Ajay Bura <ajbura@gmail.com>
2022-03-02 17:16:11 +0530

    Add navigation flow in room view model
    Signed-off-by: Ajay Bura <ajbura@gmail.com>

--- src/viewModels/session/room/RoomViewModel.ts
+++ src/viewModels/session/room/RoomViewModel.ts
@@ -8,6 +8,8 @@ import {
 } from 'hydrogen-view-sdk';
 import { colorMXID } from '../../colorMXID';
 
+type RoomFlowType = 'preview' | 'load' | 'loaded';
+
 type Options = {
   room: typeof Room
   platform: typeof Platform
@@ -18,21 +20,64 @@ type Options = {
 
 export class RoomViewModel extends ViewModel {
   private _chatViewModel: typeof ChatViewModel | null;
+  private _roomFlow: RoomFlowType;
   
   constructor(options: Options) {
     super(options);
     this.room = options.room;
     this._chatViewModel = null;
+    this._roomFlow = 'preview';
+    this.emitChange('roomFlow');
+
+    this._setupNavigation();
   }
 
-  async load() {
+  private _setupNavigation() {
+    const lpObserver = this.navigation.observe('left-panel');
+    const unSubscriber = lpObserver.subscribe(() => {
+      const lpState = lpObserver.get();
+      if (lpState === 'initial') this.setRoomFlow('preview');
+      if (this.roomFlow === 'load' && lpState !== 'close') this.setRoomFlow('preview');
+    });
+    this.track(unSubscriber);
+  }
+  
+  async loadChatView() {
     this._chatViewModel = new ChatViewModel(this.childOptions({
       room: this.room,
     }));
     this.track(this._chatViewModel);
-    this._chatViewModel.load();
+    await this._chatViewModel.load();
   }
 
+  private _disposeChatView() {
+    this.disposeTracked(this._chatViewModel);
+    this._chatViewModel = null;
+  }
+
+  setRoomFlow(flow: RoomFlowType) {
+    if (this._roomFlow === flow) return;
+
+    if (flow === 'preview') {
+      this.toggleLeftPanel('initial');
+      if (this._chatViewModel) this._disposeChatView();      
+    } else if (flow === 'load') {
+      this.toggleLeftPanel('close');
+      this.loadChatView().then(() => this.emitChange('chatViewModel'));
+    }
+
+    this._roomFlow = flow;
+    this.emitChange('roomFlow');
+  }
+
+  toggleLeftPanel(state?: 'initial' | 'open' | 'close') {
+    const lpState = this.navigation.path.get('left-panel')?.value;
+    const stateValue = state ?? (lpState === 'close' ? 'open' : 'close');
+
+    const segment = this.navigation.segment('left-panel', stateValue);
+    const path = this.navigation.path.replace(segment);
+    this.navigation.applyPath(path);
+  }
   
   getRoomColor() {
     const { avatarColorId } = this.room;
@@ -47,6 +92,10 @@ export class RoomViewModel extends ViewModel {
       return mediaRepository.mxcUrlThumbnail(avatarUrl, imageSize, imageSize, "crop");
     }
     return null;
+  }
+
+  get roomFlow() {
+    return this._roomFlow;
   }
 
   get name() {
