634949b4b4e67a3f8583d6c4b46b8035192f79cc
Ajay Bura <ajbura@gmail.com>
2022-03-02 17:22:28 +0530

    Refactor session view to prevent re-renders
    Signed-off-by: Ajay Bura <ajbura@gmail.com>

--- src/ui/views/session/SessionView.tsx
+++ src/ui/views/session/SessionView.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import './SessionView.css';
 
 import { SessionViewModel } from '../../../viewModels/session/SessionViewModel';
+import { RoomViewModel } from '../../../viewModels/session/room/RoomViewModel';
 
-import { Avatar } from '../../atoms/avatar/Avatar';
-import { Button } from '../../atoms/button/Button';
 import { LeftPanelView } from './leftpanel/LeftPanelView';
+import { RoomPreview } from "./room/RoomPreview";
+import { RoomLoading } from "./room/RoomLoading";
 import { RoomView } from './room/RoomView';
-import { InviteView } from './room/InviteView';
 
 import { useVMProp } from '../../hooks/useVMProp';
 
@@ -16,27 +16,31 @@ interface ISessionView {
 };
 
 export function SessionView({ vm }: ISessionView) {
-  const activeSection = useVMProp(vm, 'activeSection');
-  const activeRoomId = useVMProp(vm, 'activeRoomId');
-
   return (
     <div className="SessionView flex">
       <LeftPanelView vm={vm.leftPanelViewModel} />
-      <div className="grow flex flex-column">
-        <header className="flex items-center justify-end">
-          <Button variant="primary" onClick={() => alert('create room')}>Create room</Button>
-          <Avatar
-            isCircle
-            size="large"
-            name="Test"
-            imageSrc="https://images.unsplash.com/photo-1642773156765-6f5e392dec03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-            bgColor="var(--bg-primary)"
-          />
-        </header>
-        {activeSection === 'room' && <RoomView roomId={activeRoomId} vm={vm.roomViewModel!} />}
-        {activeSection === 'invite' && <InviteView vm={vm.inviteViewModel!} />}
-        {activeSection === 'none' && <p>select room from panels</p>}
-      </div>
+      <MiddleView vm={vm} />
     </div>
   );
 }
+
+function MiddleView({ vm }: { vm: SessionViewModel }) {
+  const activeRoomId = useVMProp(vm, 'activeRoomId');
+
+  if (!activeRoomId) return <p>select room from left panel</p>;
+  if (vm.isActiveRoomInvite) return <p>invite</p>;
+  return <RoomViewWrapper vm={vm.roomViewModel!} roomId={activeRoomId} />;
+}
+
+function RoomViewWrapper({ vm, roomId }: { vm: RoomViewModel, roomId: string}) {
+  const roomFlow = useVMProp(vm, 'roomFlow');
+
+  return (
+    <>
+      {vm.roomFlow === 'preview' && <RoomPreview vm={vm} roomId={roomId} />}
+      {vm.roomFlow === 'load' && <RoomLoading vm={vm} />}
+      {vm.roomFlow === 'loaded' && <RoomView vm={vm} roomId={roomId} />}
+    </>
+  );
+}
+
