6d37bbf59de5c9aa90c19259bc5eb222874156a4
Ajay Bura <ajbura@gmail.com>
2022-04-04 16:42:26 +0530

    redesign room preview
    Signed-off-by: Ajay Bura <ajbura@gmail.com>

--- src/ui/views/session/room/RoomPreview.tsx
+++ src/ui/views/session/room/RoomPreview.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-
 import "./RoomPreview.css";
 import { RoomViewModel } from "../../../../viewModels/session/room/RoomViewModel";
 import { InviteViewModel } from "../../../../viewModels/session/room/InviteViewModel";
 import { Text } from "../../../atoms/text/Text";
-import { Avatar } from "../../../atoms/avatar/Avatar";
+import { Icon } from "../../../atoms/icon/Icon";
 import { Button } from "../../../atoms/button/Button";
-import { Thumbnail } from "../../../atoms/thumbnail/Thumbnail";
-import { Scroll } from "../../../atoms/scroll/Scroll";
+import PeoplesIC from "../../../../../res/ic/peoples.svg";
 
 interface IRoomPreview {
   vm: RoomViewModel | InviteViewModel;
@@ -17,31 +14,28 @@ interface IRoomPreview {
 export function RoomPreview({ vm, roomId }: IRoomPreview) {
   const roomName = vm.name || "Empty room";
 
+  const isPreview = vm.roomFlow === "preview";
   return (
-    <div className="RoomPreview grow flex flex-column">
-      <header className="flex items-center justify-end">
-        <Button variant="primary" onClick={() => alert("create room")}>
-          Create room
-        </Button>
-        <Avatar
-          isCircle
-          size="large"
-          name="Test"
-          imageSrc="https://images.unsplash.com/photo-1642773156765-6f5e392dec03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-          bgColor="var(--bg-primary)"
-        />
-      </header>
-      <Scroll className="grow">
-        <div className="RoomPreview__content">
-          <Thumbnail size="large" name={roomName} bgColor={vm.getRoomColor()} imageSrc={vm.getRoomAvatarHttpUrl()} />
-          <Text variant="h2" weight="semi-bold">
+    <div className="RoomPreview grow flex flex-column justify-end items-center">
+      <div className="RoomPreview__card flex items-center">
+        <div className="grow">
+          <Text className="truncate" variant="s1" weight="semi-bold">
             {roomName}
           </Text>
-          <Button variant="primary" onClick={() => vm.setRoomFlow("load")}>
-            Join room
+        </div>
+        <div className="RoomPreview__card-memberCount flex items-center">
+          <Icon src={PeoplesIC} />
+          {vm.room.joinedMemberCount}
+        </div>
+        <div className="shrink-0">
+          <Button
+            variant={isPreview ? "secondary" : "primary"}
+            onClick={() => vm.setRoomFlow(isPreview ? "load" : "loaded")}
+          >
+            {isPreview ? "Load World" : "Open World"}
           </Button>
         </div>
-      </Scroll>
+      </div>
     </div>
   );
 }
