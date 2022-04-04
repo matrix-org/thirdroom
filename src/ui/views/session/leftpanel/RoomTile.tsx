1df5bb27fcb852c940dd6cf8d8c4d5ca0c92bf4f
Ajay Bura <ajbura@gmail.com>
2022-04-04 13:29:18 +0530

    Redesign left panel
    Signed-off-by: Ajay Bura <ajbura@gmail.com>

--- src/ui/views/session/leftpanel/RoomTile.tsx
+++ src/ui/views/session/leftpanel/RoomTile.tsx
@@ -1,24 +1,23 @@
 import "./RoomTile.css";
 
-import { Text } from "../../../atoms/text/Text";
-import { Thumbnail } from "../../../atoms/thumbnail/Thumbnail";
-
 interface IRoomTile {
-  name: string;
-  roomColor: string;
-  avatarUrl: string;
+  title: React.ReactNode;
+  avatar: React.ReactNode;
   isActive?: boolean;
   onClick: () => void;
 }
 
-export function RoomTile({ name, roomColor, avatarUrl, isActive = false, onClick }: IRoomTile) {
+export function RoomTile({
+  title,
+  avatar,
+  isActive = false,
+  onClick
+}: IRoomTile) {
   return (
     <button onClick={onClick} className={`RoomTile${isActive ? "--active" : ""} flex items-start`}>
-      <Thumbnail className="shrink-0" name={name} bgColor={roomColor} imageSrc={avatarUrl} />
-      <div className="RoomTile__info grow flex flex-column">
-        <Text className="truncate" variant="b2" weight="semi-bold">
-          {name}
-        </Text>
+      {avatar}
+      <div className="RoomTile__content grow flex flex-column">
+        {title}
       </div>
     </button>
   );
