501bf085d3729b159a5dd3ee8f8c5b329da823e3
Robert Long <robert@robertlong.me>
2022-04-07 19:06:12 -0700

    Reduce physics objects to 100

--- src/game.ts
+++ src/game.ts
@@ -104,7 +104,7 @@ export async function init(state: GameState): Promise<void> {
     geometryType: GeometryType.Box,
   });
 
-  for (let i = 0; i < 2000; i++) {
+  for (let i = 0; i < 100; i++) {
     const cube = createCube(state, geometryResourceId);
 
     const position = Transform.position[cube];
@@ -127,8 +127,7 @@ export async function init(state: GameState): Promise<void> {
   }
 
   const gltfEid = createGLTFEntity(state, "/gltf/OutdoorFestival/OutdoorFestival.glb");
-
-  setTimeout(() => addChild(scene, gltfEid), 3000);
+  addChild(scene, gltfEid);
 
   const playerRig = createPlayerRig(state);
   addChild(scene, playerRig);
