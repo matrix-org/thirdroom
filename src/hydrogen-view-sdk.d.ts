66bd86f42c51a8c16964d733daff7f39f6b8eaa7
Robert Long <robert@robertlong.me>
2022-04-19 13:27:39 -0700

    Fix Olm types

--- src/hydrogen-view-sdk.d.ts
+++ src/hydrogen-view-sdk.d.ts
@@ -1,4 +1,116 @@
+declare module "@matrix-org/olm" {
+  export class Account {
+    constructor();
+    free(): void;
+    create(): void;
+    identity_keys(): string;
+    sign(message: string | Uint8Array): string;
+    one_time_keys(): string;
+    mark_keys_as_published(): void;
+    max_number_of_one_time_keys(): number;
+    generate_one_time_keys(number_of_keys: number): void;
+    remove_one_time_keys(session: Session): void;
+    generate_fallback_key(): void;
+    fallback_key(): string;
+    pickle(key: string | Uint8Array): string;
+    unpickle(key: string | Uint8Array, pickle: string): void;
+  }
+
+  export class Session {
+    constructor();
+    free(): void;
+    pickle(key: string | Uint8Array): string;
+    unpickle(key: string | Uint8Array, pickle: string): void;
+    create_outbound(account: Account, their_identity_key: string, their_one_time_key: string): void;
+    create_inbound(account: Account, one_time_key_message: string): void;
+    create_inbound_from(account: Account, identity_key: string, one_time_key_message: string): void;
+    session_id(): string;
+    has_received_message(): boolean;
+    matches_inbound(one_time_key_message: string): boolean;
+    matches_inbound_from(identity_key: string, one_time_key_message: string): boolean;
+    encrypt(plaintext: string): object;
+    decrypt(message_type: number, message: string): string;
+    describe(): string;
+  }
+
+  export class Utility {
+    constructor();
+    free(): void;
+    sha256(input: string | Uint8Array): string;
+    ed25519_verify(key: string, message: string | Uint8Array, signature: string): void;
+  }
+
+  export class InboundGroupSession {
+    constructor();
+    free(): void;
+    pickle(key: string | Uint8Array): string;
+    unpickle(key: string | Uint8Array, pickle: string): void;
+    create(session_key: string): string;
+    import_session(session_key: string): string;
+    decrypt(message: string): object;
+    session_id(): string;
+    first_known_index(): number;
+    export_session(message_index: number): string;
+  }
+
+  export class OutboundGroupSession {
+    constructor();
+    free(): void;
+    pickle(key: string | Uint8Array): string;
+    unpickle(key: string | Uint8Array, pickle: string): void;
+    create(): void;
+    encrypt(plaintext: string): string;
+    session_id(): string;
+    session_key(): string;
+    message_index(): number;
+  }
+
+  export class PkEncryption {
+    constructor();
+    free(): void;
+    set_recipient_key(key: string): void;
+    encrypt(plaintext: string): object;
+  }
+
+  export class PkDecryption {
+    constructor();
+    free(): void;
+    init_with_private_key(key: Uint8Array): string;
+    generate_key(): string;
+    get_private_key(): Uint8Array;
+    pickle(key: string | Uint8Array): string;
+    unpickle(key: string | Uint8Array, pickle: string): string;
+    decrypt(ephemeral_key: string, mac: string, ciphertext: string): string;
+  }
+
+  export class PkSigning {
+    constructor();
+    free(): void;
+    init_with_seed(seed: Uint8Array): string;
+    generate_seed(): Uint8Array;
+    sign(message: string): string;
+  }
+
+  export class SAS {
+    constructor();
+    free(): void;
+    get_pubkey(): string;
+    set_their_key(their_key: string): void;
+    generate_bytes(info: string, length: number): Uint8Array;
+    calculate_mac(input: string, info: string): string;
+    calculate_mac_long_kdf(input: string, info: string): string;
+  }
+
+  export function init(opts?: object): Promise<void>;
+
+  export function get_library_version(): [number, number, number];
+
+  export const PRIVATE_KEY_LENGTH: number;
+}
+
 declare module "@thirdroom/hydrogen-view-sdk" {
+  import type * as Olm from "@matrix-org/olm";
+
   export type SubscriptionHandle = () => undefined;
 
   export abstract class BaseObservable<T> {
@@ -1910,8 +2022,6 @@ declare module "@thirdroom/hydrogen-view-sdk" {
     remove(roomId: string): void;
   }
 
-  import type * as OlmNamespace from "@matrix-org/olm";
-  type Olm = typeof OlmNamespace;
   type ClaimedOTKResponse = {
     [userId: string]: {
       [deviceId: string]: {
@@ -1941,7 +2051,7 @@ declare module "@thirdroom/hydrogen-view-sdk" {
     constructor(
       account: Account,
       pickleKey: string,
-      olm: Olm,
+      olm: typeof Olm,
       storage: Storage,
       now: () => number,
       ownUserId: string,
