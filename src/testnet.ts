93ef8f1bcdf96b8c06ad39cc778af81b00a788ed
Robert Long <robert@robertlong.me>
2022-04-27 18:02:15 -0700

    Register testnet server as vite plugin

--- src/testnet.ts
+++ src/testnet.ts
@@ -1,4 +1,5 @@
-import WebSocket, { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
+import { Plugin } from "vite";
 
 function generateUUID() {
   let d = new Date().getTime();
@@ -16,74 +17,87 @@ function generateUUID() {
   });
 }
 
-const host = "localhost";
-const port = 8080;
-const wss = new WebSocketServer({ host, port });
+function initTestnetServer() {
+  const host = "localhost";
+  const port = 9090;
+  const wss = new WebSocketServer({ host, port });
 
-let connections = 0;
-let peerHost: WebSocket | undefined;
-const peers: { [key: string]: WebSocket } = {};
-const peerIds = new Map();
-const informedPeerIds = new Map();
+  let connections = 0;
+  let peerHost: WebSocket | undefined;
+  const peers: { [key: string]: WebSocket } = {};
+  const peerIds = new Map();
+  const informedPeerIds = new Map();
 
-wss.on("connection", (ws) => {
-  connections++;
+  wss.on("connection", (ws) => {
+    connections++;
 
-  ws.binaryType = "arraybuffer";
+    ws.binaryType = "arraybuffer";
 
-  const peerId = generateUUID();
-  peers[peerId] = ws;
-  peerIds.set(ws, peerId);
-  informedPeerIds.set(peerId, new Set());
+    const peerId = generateUUID();
+    peers[peerId] = ws;
+    peerIds.set(ws, peerId);
+    informedPeerIds.set(peerId, new Set());
 
-  console.log("new client", peerId);
+    console.log("new client", peerId);
 
-  if (connections === 1) {
-    ws.send("setHost");
-    console.log("setHost", peerId);
-    peerHost = ws;
-  }
-
-  ws.send(JSON.stringify({ setPeerId: peerId }));
-
-  if (connections > 1) {
-    // inform existing peers of new peerId
-    const informed = informedPeerIds.get(peerId);
-    for (const peerIdA in peers) {
-      const alreadyInformed = informedPeerIds.get(peerId)?.has(peerIdA);
-      if (alreadyInformed) continue;
-
-      // no need to send peerId to itself
-      if (peerId === peerIdA) continue;
-
-      ws.send(JSON.stringify({ addPeerId: peerIdA }));
-      peers[peerIdA].send(JSON.stringify({ addPeerId: peerId }));
-      informed.add(peerIdA);
-
-      console.log("informed", peerId, "and", peerIdA, "of one another");
+    if (connections === 1) {
+      ws.send("setHost");
+      console.log("setHost", peerId);
+      peerHost = ws;
     }
-  }
 
-  ws.on("close", () => {
-    console.log("closed connection", peerId);
-    connections--;
-    if (connections === 0) peerHost = undefined;
-    delete peers[peerId];
-    informedPeerIds.delete(peerId);
-    if (ws === peerHost) {
-      console.log("host changed to", peerId);
-      peerHost = peers[Object.keys(peers)[0]];
+    ws.send(JSON.stringify({ setPeerId: peerId }));
+
+    if (connections > 1) {
+      // inform existing peers of new peerId
+      const informed = informedPeerIds.get(peerId);
+      for (const peerIdA in peers) {
+        const alreadyInformed = informedPeerIds.get(peerId)?.has(peerIdA);
+        if (alreadyInformed) continue;
+
+        // no need to send peerId to itself
+        if (peerId === peerIdA) continue;
+
+        ws.send(JSON.stringify({ addPeerId: peerIdA }));
+        peers[peerIdA].send(JSON.stringify({ addPeerId: peerId }));
+        informed.add(peerIdA);
+
+        console.log("informed", peerId, "and", peerIdA, "of one another");
+      }
     }
-  });
 
-  ws.on("message", (data, isBinary) => {
-    // console.log("new msg", data, isBinary);
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
+    ws.on("close", () => {
+      console.log("closed connection", peerId);
+      connections--;
+      if (connections === 0) peerHost = undefined;
+      delete peers[peerId];
+      informedPeerIds.delete(peerId);
+      if (ws === peerHost) {
+        console.log("host changed to", peerId);
+        peerHost = peers[Object.keys(peers)[0]];
       }
     });
-  });
-});
 
-console.log(`websocket server started at ws://${host}:${port}`);
+    ws.on("message", (data, isBinary) => {
+      // console.log("new msg", data, isBinary);
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data, { binary: isBinary });
+        }
+      });
+    });
+  });
+
+  console.log(`Testnet websocket server started at ws://${host}:${port}`);
+}
+
+export default function TestnetServerPlugin(): Plugin {
+  return {
+    name: "testnet-server",
+    configureServer() {
+      if (process.env.VITE_USE_TESTNET) {
+        initTestnetServer();
+      }
+    },
+  };
+}
