2cf6dc719cfefc55e905d1ec9bcee954ff9030ec
Ajay Bura <ajbura@gmail.com>
2022-05-06 08:58:50 +0530

    Add after and before to input component

--- src/ui/atoms/input/Input.tsx
+++ src/ui/atoms/input/Input.tsx
@@ -8,6 +8,7 @@ interface IInput {
   name?: string;
   placeholder?: string;
   type?: "text" | "password" | "email" | "number" | "search";
+  inputSize?: "sm" | "md" | "lg";
   state?: "success" | "error";
   value?: string;
   defaultValue?: string;
@@ -18,6 +19,8 @@ interface IInput {
   autoFocus?: boolean;
   readonly?: boolean;
   size?: number;
+  before?: React.ReactNode;
+  after?: React.ReactNode;
 }
 
 export function Input({
@@ -26,6 +29,7 @@ export function Input({
   name,
   placeholder,
   type = "text",
+  inputSize = "md",
   state,
   value,
   defaultValue,
@@ -36,28 +40,34 @@ export function Input({
   autoFocus = false,
   readonly,
   size = 1,
+  before,
+  after,
 }: IInput) {
-  const inputClass = classNames("Input", className, {
+  const inputClass = classNames(`Input Input--${inputSize}`, className, {
     "Input--success": state === "success",
     "Input--error": state === "error",
+    "Input--disabled": disabled,
   });
 
   return (
-    <input
-      className={inputClass}
-      id={id}
-      name={name}
-      placeholder={placeholder}
-      type={type}
-      value={value}
-      defaultValue={defaultValue}
-      onChange={onChange}
-      disabled={disabled}
-      required={required}
-      autoComplete={autoComplete}
-      autoFocus={autoFocus}
-      readOnly={readonly}
-      size={size}
-    />
+    <div className={inputClass}>
+      {before}
+      <input
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        defaultValue={defaultValue}
+        onChange={onChange}
+        disabled={disabled}
+        required={required}
+        autoComplete={autoComplete}
+        autoFocus={autoFocus}
+        readOnly={readonly}
+        size={size}
+      />
+      {after}
+    </div>
   );
 }
