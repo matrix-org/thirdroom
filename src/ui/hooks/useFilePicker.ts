8f193d6c4eb49ac6ab82e374b8a7d1ed127fbdfe
Ajay Bura <ajbura@gmail.com>
2022-07-07 14:11:17 +0530

    Add file picker hook

---
+++ src/ui/hooks/useFilePicker.ts
@@ -0,0 +1,35 @@
+import { useCallback, useEffect, useState } from "react";
+import { IBlobHandle, Platform } from "@thirdroom/hydrogen-view-sdk";
+
+export function useFilePicker(platform: Platform, mimeType: string) {
+  const [fileData, setFileData] = useState<{ blob?: IBlobHandle; url?: string; pickUsed: number; dropUsed: number }>({
+    pickUsed: 0,
+    dropUsed: 0,
+  });
+
+  useEffect(() => {
+    return () => {
+      if (fileData.url) {
+        URL.revokeObjectURL(fileData.url);
+      }
+    };
+  }, [fileData.url]);
+
+  const pickFile = useCallback(async () => {
+    const data = await platform.openFile(mimeType);
+    if (!data) return;
+    setFileData((state) => ({
+      blob: data.blob,
+      url: URL.createObjectURL(data.blob.nativeBlob),
+      pickUsed: state.pickUsed + 1,
+      dropUsed: state.dropUsed,
+    }));
+  }, [setFileData, platform, mimeType]);
+
+  const dropFile = useCallback(
+    () => setFileData((state) => ({ pickUsed: state.pickUsed, dropUsed: state.dropUsed + 1 })),
+    []
+  );
+
+  return { fileData, pickFile, dropFile };
+}
