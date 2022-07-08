ade9778b14ae325e330f5a679094120c1af58982
Ajay Bura <ajbura@gmail.com>
2022-07-08 15:30:00 +0530

    Add file upload card component

---
+++ src/ui/views/components/file-upload-card/FileUploadCard.tsx
@@ -0,0 +1,40 @@
+import classNames from "classnames";
+
+import { Text } from "../../../atoms/text/Text";
+import { IconButton } from "../../../atoms/button/IconButton";
+import { Progress } from "../../../atoms/progress/Progress";
+import { bytesToSize, getPercentage } from "../../../utils/common";
+import CrossIC from "../../../../../res/ic/cross.svg";
+import "./FileUploadCard.css";
+
+interface FileUploadCardProps {
+  className?: string;
+  name: string;
+  totalBytes: number;
+  sentBytes: number;
+  onUploadDrop: () => void;
+}
+
+export function FileUploadCard({ className, name, sentBytes, totalBytes, onUploadDrop }: FileUploadCardProps) {
+  return (
+    <div className={classNames("FileUploadCard flex flex-column gap-xs", className)}>
+      <div className="flex items-center gap-xs">
+        <div className="grow">
+          <Text className="truncate" variant="b2" weight="medium">
+            {name}
+          </Text>
+        </div>
+        <IconButton onClick={onUploadDrop} iconSrc={CrossIC} size="sm" label="cancel" />
+      </div>
+      <Progress variant="secondary" value={sentBytes} max={totalBytes} />
+      <div className="flex items-center gap-xs">
+        <Text className="grow" variant="b3" color="surface-low">
+          {sentBytes < totalBytes ? `${getPercentage(totalBytes, sentBytes)}%` : "Completed"}
+        </Text>
+        <Text variant="b3" color="surface-low">
+          {bytesToSize(sentBytes)} / {bytesToSize(totalBytes)}
+        </Text>
+      </div>
+    </div>
+  );
+}
