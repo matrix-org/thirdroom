8a236e08710c0cabbb62b8ef25ed4f3e539af74c
Ajay Bura <ajbura@gmail.com>
2022-07-15 12:44:24 +0530

    Add user menu in space bar

--- src/ui/views/session/sidebar/SpacesView.tsx
+++ src/ui/views/session/sidebar/SpacesView.tsx
@@ -2,23 +2,19 @@ import { SidebarTab } from "../../components/sidebar-tab/SidebarTab";
 import PlanetIC from "../../../../../res/ic/planet.svg";
 import PeoplesIC from "../../../../../res/ic/peoples.svg";
 import NotificationIC from "../../../../../res/ic/notification.svg";
-import { DropdownMenu } from "../../../atoms/menu/DropdownMenu";
-import { DropdownMenuItem } from "../../../atoms/menu/DropdownMenuItem";
-import { Avatar } from "../../../atoms/avatar/Avatar";
-import { getAvatarHttpUrl, getIdentifierColorNumber } from "../../../utils/avatar";
 import { useHydrogen } from "../../../hooks/useHydrogen";
-import { OverlayWindow, SidebarTabs, useStore } from "../../../hooks/useStore";
+import { SidebarTabs, useStore } from "../../../hooks/useStore";
 import { BadgeWrapper } from "../../../atoms/badge/BadgeWrapper";
 import { NotificationBadge } from "../../../atoms/badge/NotificationBadge";
 import { useInviteList } from "../../../hooks/useInviteList";
+import { UserMenu } from "../menus/UserMenu";
 import "./SpacesView.css";
 
 export function SpacesView() {
-  const { session, platform, logout } = useHydrogen(true);
+  const { session } = useHydrogen(true);
   const invites = useInviteList(session);
-  const { userId, displayName, avatarUrl } = useStore((state) => state.userProfile);
   const { selectedSidebarTab, selectSidebarTab } = useStore((state) => state.overlaySidebar);
-  const { selectedWindow, selectWindow } = useStore((state) => state.overlayWindow);
+  const { selectedWindow } = useStore((state) => state.overlayWindow);
 
   return (
     <div className="SpacesView flex flex-column">
@@ -50,24 +46,7 @@ export function SpacesView() {
         </BadgeWrapper>
       </div>
       <div className="shrink-0 flex flex-column items-center gap-xs">
-        <DropdownMenu
-          content={
-            <>
-              <DropdownMenuItem onSelect={() => selectWindow(OverlayWindow.UserProfile)}>View Profile</DropdownMenuItem>
-              <DropdownMenuItem onSelect={logout} variant="danger">
-                Logout
-              </DropdownMenuItem>
-            </>
-          }
-        >
-          <Avatar
-            onClick={() => false /* used for keyboard focus */}
-            shape="circle"
-            name={displayName}
-            bgColor={`var(--usercolor${getIdentifierColorNumber(userId)})`}
-            imageSrc={avatarUrl ? getAvatarHttpUrl(avatarUrl, 40, platform, session.mediaRepository) : undefined}
-          />
-        </DropdownMenu>
+        <UserMenu />
       </div>
     </div>
   );
