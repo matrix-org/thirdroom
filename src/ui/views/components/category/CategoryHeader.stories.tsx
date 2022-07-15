3dbe522931a68ff38810d8fbaeae157d2ed6c637
Ajay Bura <ajbura@gmail.com>
2022-07-15 14:13:09 +0530

    Update category header component & add Icons

--- src/ui/views/components/category/CategoryHeader.stories.tsx
+++ src/ui/views/components/category/CategoryHeader.stories.tsx
@@ -1,15 +1,20 @@
 import { CategoryHeader } from "./CategoryHeader";
 import { IconButton } from "../../../atoms/button/IconButton";
+import { Icon } from "../../../atoms/icon/Icon";
 import StarIC from "../../../../../res/ic/star.svg";
+import ChevronRightIC from "../../../../../res/ic/chevron-right.svg";
 import AddIC from "../../../../../res/ic/add.svg";
 import MoreHorizontalIC from "../../../../../res/ic/more-horizontal.svg";
 
 export const title = "CategoryHeader";
 
 export default function CategoryHeaderStories() {
+  const icon = <Icon src={StarIC} size="sm" color="surface" />;
+  const icon1 = <Icon src={ChevronRightIC} size="sm" color="surface" />;
+
   return (
     <div style={{ backgroundColor: "white", maxWidth: "380px" }}>
-      <CategoryHeader iconSrc={StarIC} title="Favorite Worlds" />
+      <CategoryHeader before={icon} title="Favorite Worlds" />
 
       <CategoryHeader
         title="All Messages"
@@ -22,8 +27,9 @@ export default function CategoryHeaderStories() {
       />
 
       <CategoryHeader
-        iconSrc={StarIC}
+        before={icon}
         title="Favorite Rooms"
+        after={icon1}
         onClick={() => console.log("hello")}
         options={
           <>
