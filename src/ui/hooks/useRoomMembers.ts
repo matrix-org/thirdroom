c988418cef2cd8041ba79b791a6fc56e76a5c9da
Ajay Bura <ajbura@gmail.com>
2022-07-26 09:05:30 +0530

    update room member hook

--- src/ui/hooks/useRoomMembers.ts
+++ src/ui/hooks/useRoomMembers.ts
@@ -1,28 +1,53 @@
 import { useEffect, useState } from "react";
-import { MemberList, ObservableMap, Room, RoomMember } from "@thirdroom/hydrogen-view-sdk";
+import { ObservableMap, Room, RoomMember } from "@thirdroom/hydrogen-view-sdk";
 
-import { useObservableMap } from "./useObservableMap";
 import { useIsMounted } from "./useIsMounted";
 
+interface MembersType {
+  invited: RoomMember[];
+  joined: RoomMember[];
+  leaved: RoomMember[];
+  banned: RoomMember[];
+}
+
 export function useRoomMembers(room: Room) {
-  const [memberList, setMemberList] = useState<MemberList>();
-  const members = useObservableMap(
-    () => memberList?.members || new ObservableMap<string, RoomMember>(),
-    [memberList?.members]
-  );
+  const [members, setMembers] = useState<MembersType>();
   const isMounted = useIsMounted();
 
-  useEffect(() => {
-    return () => {
-      memberList?.release();
+  const getMembers = (m: ObservableMap<string, RoomMember>) => {
+    const members: MembersType = {
+      invited: [],
+      joined: [],
+      leaved: [],
+      banned: [],
     };
-  }, [memberList]);
+    for (const [, member] of m) {
+      if (member.membership === "invite") members.invited.push(member);
+      else if (member.membership === "join") members.joined.push(member);
+      else if (member.membership === "leave") members.leaved.push(member);
+      else if (member.membership === "ban") members.banned.push(member);
+    }
+    return members;
+  };
 
   useEffect(() => {
+    let unSub: () => void | undefined;
     room.loadMemberList().then((mList) => {
       if (!isMounted()) return;
-      setMemberList(mList);
+      const { members } = mList;
+      setMembers(getMembers(members));
+
+      unSub = members.subscribe({
+        onReset: () => setMembers(getMembers(members)),
+        onAdd: () => setMembers(getMembers(members)),
+        onUpdate: () => setMembers(getMembers(members)),
+        onRemove: () => setMembers(getMembers(members)),
+      });
     });
+
+    return () => {
+      unSub?.();
+    };
   }, [room, isMounted]);
 
   return members;
