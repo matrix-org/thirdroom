2c7ffdd371b00d2b195485962096ddc4b8f73988
Nate Martin <natem@element.io>
2022-08-10 16:07:14 -0700

    cleanup

--- src/ui/views/session/reticle/Reticle.tsx
+++ src/ui/views/session/reticle/Reticle.tsx
@@ -11,12 +11,10 @@ import "./Reticle.css";
 export function Reticle() {
   const ctx = useMainThreadContext();
   const [focused, setFocused] = useState<boolean>(false);
-  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     const onReticleFocus = (ctx: IMainThreadContext, message: ReticleFocusMessageType) => {
       setFocused(message.focused);
-      setError(message.error || false);
     };
 
     return registerMessageHandler(ctx, ReticleFocusMessage, onReticleFocus);
@@ -26,8 +24,7 @@ export function Reticle() {
     <div
       className={classNames("Reticle", {
         "Reticle--focused": focused,
-        Reticle__blue: focused && !error,
-        Reticle__red: focused && error,
+        Reticle__blue: focused,
       })}
     />
   );
