be3402c9b867d38d3e0c70959929e15a09887dd1
Nate Martin <natem@element.io>
2022-08-10 16:11:48 -0700

    reticle click

--- src/ui/views/session/reticle/Reticle.tsx
+++ src/ui/views/session/reticle/Reticle.tsx
@@ -11,6 +11,7 @@ import "./Reticle.css";
 export function Reticle() {
   const ctx = useMainThreadContext();
   const [focused, setFocused] = useState<boolean>(false);
+  const [mouseDown, setMouseDown] = useState<boolean>(false);
 
   useEffect(() => {
     const onReticleFocus = (ctx: IMainThreadContext, message: ReticleFocusMessageType) => {
@@ -20,11 +21,19 @@ export function Reticle() {
     return registerMessageHandler(ctx, ReticleFocusMessage, onReticleFocus);
   }, [ctx]);
 
+  document.addEventListener("mousedown", () => {
+    setMouseDown(true);
+  });
+  document.addEventListener("mouseup", () => {
+    setMouseDown(false);
+  });
+
   return (
     <div
       className={classNames("Reticle", {
         "Reticle--focused": focused,
         Reticle__blue: focused,
+        "Reticle--mousedown": mouseDown,
       })}
     />
   );
