ce964e4cb5c293564b1a271348ffb05817d1eaff
Nate Martin <natem@element.io>
2022-08-11 20:37:43 -0700

    focus bugfix

--- src/plugins/reticle/reticle.game.ts
+++ src/plugins/reticle/reticle.game.ts
@@ -1,5 +1,5 @@
 import RAPIER from "@dimforge/rapier3d-compat";
-import { defineComponent, defineQuery, enterQuery, exitQuery, addComponent, removeComponent } from "bitecs";
+import { defineComponent, defineQuery, exitQuery, addComponent, removeComponent } from "bitecs";
 import { vec3, quat, mat4 } from "gl-matrix";
 import { Quaternion, Vector3 } from "three";
 
@@ -13,7 +13,7 @@ import { ReticleFocusMessage } from "./reticle.common";
 
 const FocusComponent = defineComponent();
 const focusQuery = defineQuery([FocusComponent]);
-const enterFocusQuery = enterQuery(focusQuery);
+// const enterFocusQuery = enterQuery(focusQuery);
 const exitFocusQuery = exitQuery(focusQuery);
 
 const MAX_FOCUS_DISTANCE = 2;
@@ -73,22 +73,19 @@ export function ReticleFocusSystem(ctx: GameState) {
           peerId = p;
         }
       }
+      ctx.sendMessage(Thread.Main, {
+        type: ReticleFocusMessage,
+        focused: true,
+        entityId: eid,
+        networkId: eid ? Networked.networkId[eid] : undefined,
+        prefab: eid ? Prefab.get(eid) : undefined,
+        peerId,
+      });
     }
   } else {
     // clear focus
     focusQuery(ctx.world).forEach((eid) => removeComponent(ctx.world, FocusComponent, eid));
   }
-
-  const entered = enterFocusQuery(ctx.world);
-  if (entered[0])
-    ctx.sendMessage(Thread.Main, {
-      type: ReticleFocusMessage,
-      focused: true,
-      entityId: eid,
-      networkId: eid ? Networked.networkId[eid] : undefined,
-      prefab: eid ? Prefab.get(eid) : undefined,
-      peerId,
-    });
 
   const exited = exitFocusQuery(ctx.world);
   if (exited[0]) ctx.sendMessage(Thread.Main, { type: ReticleFocusMessage, focused: false });
