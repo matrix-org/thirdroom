1fee121c0c9bd806acaa0e78aef08f8455ba9ce4
Nate Martin <natem@element.io>
2022-08-18 15:39:36 -0700

    nametag ui/ux tweaks

--- src/ui/views/session/nametags/Nametags.tsx
+++ src/ui/views/session/nametags/Nametags.tsx
@@ -23,10 +23,10 @@ import { isPeerMuted } from "../../../../engine/network/network.main";
 import { Avatar } from "../../../atoms/avatar/Avatar";
 import { getAvatarHttpUrl, getIdentifierColorNumber } from "../../../utils/avatar";
 
-const DIST_HIDE = 10;
-const DIST_SHOW = 8;
+const DIST_HIDE = 50;
+const DIST_SHOW = 10;
 
-const MIN_OPACITY = 0;
+const MIN_OPACITY = 0.2;
 const MAX_OPACITY = 1;
 
 const SPEAKING_OPACITY = 0.75;
@@ -34,13 +34,70 @@ const SILENT_OPACITY = 0.4;
 
 type SpeakingRoomMember = Member & { volumeDetector: { isSpeaking: boolean } };
 
+function Nametag({ room, nametag, groupCall }: { room: Room; nametag: [string, vec2, number]; groupCall: GroupCall }) {
+  const engine = useMainThreadContext();
+  const { joined } = useRoomMembers(room) ?? {};
+
+  const { platform, session } = useHydrogen(true);
+  const [name, [left, top], dist] = nametag;
+  const opacity = range(DIST_HIDE, DIST_SHOW, MIN_OPACITY, MAX_OPACITY, dist);
+  const member = joined?.find((m) => m.userId === name);
+
+  const memberCall = member
+    ? Array.from(new Map(groupCall.members).values()).find((m) => m.userId === member?.userId && m.isConnected)
+    : undefined;
+
+  const avatarUrl = member?.avatarUrl;
+  const userId = member?.userId || name;
+
+  const volumeDetector =
+    (memberCall && (memberCall as SpeakingRoomMember).volumeDetector) ||
+    (memberCall?.remoteMedia?.userMedia &&
+      ((memberCall as SpeakingRoomMember).volumeDetector = (platform.mediaDevices as any).createVolumeMeasurer(
+        memberCall?.remoteMedia?.userMedia,
+        () => {}
+      )));
+
+  const speaking = volumeDetector?.isSpeaking;
+
+  return (
+    <div
+      key={name}
+      style={{
+        position: "absolute",
+        transform: `translate(${left}px, ${top}px)`,
+        opacity: `${opacity}`,
+      }}
+      className="Nametag"
+    >
+      <div className="Nametag--inner Text-b3">
+        <Avatar
+          size="xxs"
+          shape="circle"
+          name={member?.displayName?.toUpperCase() || name.replace("@", "").toUpperCase()}
+          imageSrc={avatarUrl ? getAvatarHttpUrl(avatarUrl, 25, platform, session.mediaRepository) : undefined}
+          bgColor={`var(--usercolor${getIdentifierColorNumber(userId)})`}
+          className={avatarUrl ? "Nametag--image" : ""}
+        />
+        {member?.displayName || name}
+        <span>
+          <object
+            style={{ opacity: `${speaking ? SPEAKING_OPACITY : SILENT_OPACITY}` }}
+            className="Icon"
+            type="image/svg+xml"
+            data={isPeerMuted(engine, name) ? VolumeOffIC : speaking ? VolumeUpIC : VolumeMuteIC}
+          />
+        </span>
+      </div>
+    </div>
+  );
+}
+
 export function Nametags({ room, enabled }: { room: Room; enabled: boolean }) {
   const engine = useMainThreadContext();
   const [nametags, setNametags] = useState<[string, vec2, number][]>([]);
 
   engine.sendMessage<NametagsEnableMessageType>(Thread.Game, { type: NametagsEnableMessage, enabled });
-
-  if (!enabled) setNametags([]);
 
   useEffect(() => {
     const onNametagsMessage = (ctx: IMainThreadContext, message: NametagsMessageType) => {
@@ -49,12 +106,10 @@ export function Nametags({ room, enabled }: { room: Room; enabled: boolean }) {
     return registerMessageHandler(engine, NametagsMessage, onNametagsMessage);
   }, [engine, nametags]);
 
-  const { joined } = useRoomMembers(room) ?? {};
-
-  const { platform, session } = useHydrogen(true);
+  const { session } = useHydrogen(true);
   const [, world] = useWorld();
 
-  let groupCall: GroupCall;
+  let groupCall: GroupCall | undefined;
   if (world) {
     for (const [, call] of Array.from(session.callHandler.calls)) {
       if (call.roomId === world.id) {
@@ -64,58 +119,13 @@ export function Nametags({ room, enabled }: { room: Room; enabled: boolean }) {
     }
   }
 
-  const Nametag = (nametag: [string, vec2, number]) => {
-    const [name, [left, top], dist] = nametag;
-    const opacity = range(DIST_HIDE, DIST_SHOW, MIN_OPACITY, MAX_OPACITY, dist);
-    const member = joined?.find((m) => m.userId === name);
-
-    const memberCall = member
-      ? Array.from(new Map(groupCall.members).values()).find((m) => m.userId === member?.userId && m.isConnected)
-      : undefined;
-
-    const avatarUrl = member?.avatarUrl;
-    const userId = member?.userId || name;
-
-    const volumeDetector =
-      (memberCall as SpeakingRoomMember).volumeDetector ||
-      ((memberCall as SpeakingRoomMember).volumeDetector = (platform.mediaDevices as any).createVolumeMeasurer(
-        memberCall?.remoteMedia?.userMedia,
-        () => {}
-      ));
-
-    const speaking = volumeDetector.isSpeaking;
-
-    return (
-      <div
-        key={name}
-        style={{
-          position: "absolute",
-          transform: `translate(${left}px, ${top}px)`,
-          opacity: `${opacity}`,
-        }}
-        className="Nametag"
-      >
-        <div className="Nametag--inner Text-b3">
-          <Avatar
-            size="xxs"
-            shape="circle"
-            name={member?.displayName?.toUpperCase() || name.replace("@", "").toUpperCase()}
-            imageSrc={avatarUrl ? getAvatarHttpUrl(avatarUrl, 25, platform, session.mediaRepository) : undefined}
-            bgColor={`var(--usercolor${getIdentifierColorNumber(userId)})`}
-          />
-          {member?.displayName || name}
-          <span>
-            <object
-              style={{ opacity: `${speaking ? SPEAKING_OPACITY : SILENT_OPACITY}` }}
-              className="Icon"
-              type="image/svg+xml"
-              data={isPeerMuted(engine, name) ? VolumeOffIC : speaking ? VolumeUpIC : VolumeMuteIC}
-            />
-          </span>
-        </div>
-      </div>
-    );
-  };
-
-  return <div>{enabled && nametags.map(Nametag)}</div>;
+  return (
+    <div>
+      {enabled &&
+        groupCall &&
+        nametags.map((nametag) => (
+          <Nametag key={nametag[0]} room={room} nametag={nametag} groupCall={groupCall as GroupCall} />
+        ))}
+    </div>
+  );
 }
