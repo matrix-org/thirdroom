496d504d9a6b11233b5d21b1d7f4f37cc430120e
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2022-08-30 15:13:28 +0530

    Add request mic permission without joining world

--- src/ui/views/session/overlay/NowPlayingControls.tsx
+++ src/ui/views/session/overlay/NowPlayingControls.tsx
@@ -1,20 +1,34 @@
-import React from "react";
-
 import { IconButton } from "../../../atoms/button/IconButton";
 import { Tooltip } from "../../../atoms/tooltip/Tooltip";
 import MicIC from "../../../../../res/ic/mic.svg";
 import MicOffIC from "../../../../../res/ic/mic-off.svg";
 import "./NowPlayingControls.css";
-import usePermissionState from "../../../hooks/usePermissionState";
+import { usePermissionState } from "../../../hooks/usePermissionState";
+import { useMicrophone } from "../../../hooks/useMicrophone";
+import { MicPermissionRequest } from "../../components/MicPermissionRequest";
 
 export function NowPlayingControls() {
   const micPermission = usePermissionState("microphone");
+  const [microphone, toggleMicrophone] = useMicrophone(micPermission);
 
   return (
     <div className="NowPlayingControls shrink-0 flex items-center">
-      <Tooltip content={micPermission === "granted" ? "Mute" : "Unmute"}>
-        <IconButton variant="surface-low" label="Mic" iconSrc={micPermission === "granted" ? MicIC : MicOffIC} />
-      </Tooltip>
+      <MicPermissionRequest
+        permissionState={micPermission}
+        render={(requestPerm) => (
+          <Tooltip content={microphone ? "Mute" : "Unmute"}>
+            <IconButton
+              variant="surface-low"
+              label="Mic"
+              iconSrc={microphone ? MicIC : MicOffIC}
+              onClick={() => {
+                if (micPermission == "granted") toggleMicrophone();
+                else requestPerm();
+              }}
+            />
+          </Tooltip>
+        )}
+      />
     </div>
   );
 }
