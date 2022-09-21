6f1d52fe5b54b2de7bf95c78d51d09499c33b832
Robert Long <robert@robertlong.me>
2022-09-21 10:54:14 -0700

    Fix crate collision group

--- src/engine/mesh/mesh.game.ts
+++ src/engine/mesh/mesh.game.ts
@@ -19,6 +19,7 @@ import { GameState } from "../GameTypes";
 import { createRemoteStandardMaterial, RemoteMaterial } from "../material/material.game";
 import { getModule, Thread } from "../module/module.common";
 import { addRemoteNodeComponent, RemoteNode } from "../node/node.game";
+import { dynamicObjectCollisionGroups } from "../physics/CollisionGroups";
 import { PhysicsModule, addRigidBody } from "../physics/physics.game";
 import { RendererModule } from "../renderer/renderer.game";
 import { addResourceRef, createResource, disposeResource } from "../resource/resource.game";
@@ -434,8 +435,6 @@ export const createSphereMesh = (ctx: GameState, radius: number, material?: Remo
   return createMesh(ctx, geometry, material);
 };
 
-const COLLISION_GROUPS = 0xffff_ffff;
-
 export const createPhysicsCube = (ctx: GameState, size: number, material?: RemoteMaterial, remote = false) => {
   const { world } = ctx;
   const { physicsWorld } = getModule(ctx, PhysicsModule);
@@ -457,8 +456,7 @@ export const createPhysicsCube = (ctx: GameState, size: number, material?: Remot
 
   const colliderDesc = RAPIER.ColliderDesc.cuboid(size / 2, size / 2, size / 2)
     .setActiveEvents(RAPIER.ActiveEvents.CONTACT_EVENTS)
-    .setCollisionGroups(COLLISION_GROUPS)
-    .setSolverGroups(COLLISION_GROUPS);
+    .setCollisionGroups(dynamicObjectCollisionGroups);
 
   physicsWorld.createCollider(colliderDesc, rigidBody.handle);
 
