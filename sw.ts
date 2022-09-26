9eff0d77a568fa79204576cab4cbb37e95691fd7
Robert Long <robert@robertlong.me>
2022-09-26 13:07:09 -0700

    Simplify service worker and fix error handling

--- sw.ts
+++ sw.ts
@@ -5,35 +5,25 @@ export declare const self: ServiceWorkerGlobalScope;
 
 const MXC_DOWNLOAD_CACHE = "download_mxc";
 
-function getCache(request: Request, cacheName: string) {
-  return caches.open(cacheName).then((cache) => cache.match(request));
-}
-
-function fetchAndCache(request: Request, cacheName: string) {
-  return fetch(request.clone()).then((response) => {
-    if (response.status === 200) {
-      const clonedResponse = response.clone();
-      caches.open(cacheName).then((cache) => cache.put(request, clonedResponse));
-    }
-    return response;
-  });
-}
-
 self.addEventListener("fetch", async (event: FetchEvent) => {
   const { request } = event;
-  if (request.method !== "GET") return;
-  if (/^\S+\/_matrix\/media\/r0\/download\/\S+$/.test(request.url) === false) return;
+
   // Only handle mxc download request.
+  if (request.method !== "GET" || /^\S+\/_matrix\/media\/r0\/download\/\S+$/.test(request.url) === false) return;
 
   event.respondWith(
-    getCache(request, MXC_DOWNLOAD_CACHE)
-      .then((response) => {
-        if (response) return response;
-        return fetchAndCache(request, MXC_DOWNLOAD_CACHE);
-      })
-      .catch((err) => {
-        console.log(err);
-        return fetchAndCache(request, MXC_DOWNLOAD_CACHE);
-      })
+    caches.open(MXC_DOWNLOAD_CACHE).then((cache) => {
+      return cache.match(request.url).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(request).then((fetchedResponse) => {
+          cache.put(request, fetchedResponse.clone());
+
+          return fetchedResponse;
+        });
+      });
+    })
   );
 });
