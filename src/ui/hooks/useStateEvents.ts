6db39df1947e2fa010945779159432a7308882cb
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2022-11-11 10:44:55 +0530

    Update @thirdroom/hydrogen-view-sdk to v0.0.28

--- src/ui/hooks/useStateEvents.ts
+++ src/ui/hooks/useStateEvents.ts
@@ -8,7 +8,6 @@ export function useStateEvents(room: Room, eventType: string) {
   const [eventObservable, setEventObservable] = useState(new ObservableMap<string, StateEvent>());
   const isMounted = useIsMounted();
 
-  // FIXME: merge https://github.com/vector-im/hydrogen-web/commit/32835e26b9b2435dc7db32d76a82cf698ee11216 in @thirdroom/hydrogen-view-sdk
   const events = useObservableMap(() => eventObservable, [eventObservable]);
 
   useEffect(() => {
