9eca24e8dbb39af37359983f3cb0827e7af34dea
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2022-11-25 16:06:59 +0530

    Remove unused code

--- src/ui/views/session/create-world/CreateWorld.tsx
+++ src/ui/views/session/create-world/CreateWorld.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { RoomVisibility, IBlobHandle, RoomType, Session } from "@thirdroom/hydrogen-view-sdk";
 
 import { Text } from "../../../atoms/text/Text";
 import { Icon } from "../../../atoms/icon/Icon";
@@ -10,7 +9,6 @@ import { HeaderTitle } from "../../../atoms/header/HeaderTitle";
 import { WindowContent } from "../../components/window/WindowContent";
 import { WindowAside } from "../../components/window/WindowAside";
 import LanguageIC from "../../../../../res/ic/language.svg";
-import { getImageDimension } from "../../../utils/common";
 import { useHydrogen } from "../../../hooks/useHydrogen";
 import { useStore } from "../../../hooks/useStore";
 import { Content } from "../../../atoms/content/Content";
@@ -18,81 +16,6 @@ import CrossIC from "../../../../../res/ic/cross.svg";
 import { ScenePreview } from "../../components/scene-preview/ScenePreview";
 import { getHttpUrl } from "../../../utils/avatar";
 import { CreateWorldForm } from "./CreateWorldForm";
-
-export interface CreateWorldOptions {
-  avatar?: IBlobHandle;
-  name: string;
-  topic?: string;
-  visibility: RoomVisibility;
-  alias?: string;
-  content: {
-    scene_url: string;
-    scene_preview_url: string;
-    max_member_object_cap?: number;
-  } & Record<string, any>;
-}
-
-export async function createWorld(
-  session: Session,
-  { avatar, name, topic, visibility, alias, content }: CreateWorldOptions
-) {
-  const avatarInfo = !avatar
-    ? undefined
-    : {
-        name: avatar.nativeBlob.name,
-        blob: avatar,
-        info: {
-          mimetype: avatar.nativeBlob.type,
-          size: avatar.size,
-          ...(await getImageDimension(avatar.nativeBlob)),
-        },
-      };
-
-  const roomBeingCreated = await session.createRoom({
-    type: RoomType.World,
-    visibility,
-    avatar: avatarInfo,
-    name,
-    topic,
-    alias,
-    isEncrypted: false,
-    isFederationDisabled: false,
-    powerLevelContentOverride: {
-      invite: 100,
-      kick: 100,
-      ban: 100,
-      redact: 50,
-      state_default: 50,
-      events_default: 0,
-      users_default: 0,
-      events: {
-        "m.room.power_levels": 100,
-        "m.room.history_visibility": 100,
-        "m.room.tombstone": 100,
-        "m.room.encryption": 100,
-        "m.room.name": 50,
-        "m.room.message": 0,
-        "m.room.encrypted": 50,
-        "m.sticker": 50,
-        "org.matrix.msc3815.world": 50,
-        "org.matrix.msc3401.call": 0,
-        "org.matrix.msc3401.call.member": 0,
-        "org.matrix.msc3815.member.world": 0,
-      },
-      users: {
-        [session.userId]: 100,
-      },
-    },
-    initialState: [
-      {
-        type: "org.matrix.msc3815.world",
-        content,
-      },
-    ],
-  });
-
-  return roomBeingCreated;
-}
 
 export function CreateWorld() {
   const { session } = useHydrogen(true);
