535d4cb9acc284a16f3c3cc75c27e847ae0adfcd
Robert Long <robert@robertlong.me>
2022-12-02 15:48:01 -0800

    Add the interactable-example in C

---
+++ examples/interactable/c/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p ./build
+
+emcc \
+  -O3 \
+  -g \
+  --no-entry \
+  -s ALLOW_MEMORY_GROWTH=0 \
+  -s INITIAL_MEMORY=67108864 \
+  -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
+  -Wl,--import-memory \
+  -o ./build/interactable-example.wasm \
+  src/*.c \
