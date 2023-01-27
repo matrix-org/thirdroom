016a589c638b6434d21bbfe34a27def37c20c14a
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-01-27 10:08:01 +0530

    Add menu item component

---
+++ src/ui/atoms/menu/MenuItem.tsx
@@ -0,0 +1,26 @@
+import { AllHTMLAttributes, forwardRef, ReactNode } from "react";
+import classNames from "classnames";
+
+import { Text } from "../text/Text";
+import "./MenuItem.css";
+interface MenuItemProps {
+  className?: string;
+  variant?: "surface" | "primary" | "secondary" | "danger";
+  children: ReactNode;
+}
+
+export const MenuItem = forwardRef<HTMLDivElement, MenuItemProps & AllHTMLAttributes<HTMLDivElement>>(
+  ({ className, variant = "surface", children, ...props }, ref) => {
+    return (
+      <div className={classNames("MenuItem", `MenuItem--${variant}`, className)} ref={ref} {...props}>
+        {typeof children === "string" ? (
+          <Text variant="b2" color={variant} weight="medium">
+            {children}
+          </Text>
+        ) : (
+          children
+        )}
+      </div>
+    );
+  }
+);
