5e45e0dc1d339e16cd9bdda09123f44c72374f82
Robert Long <robert@robertlong.me>
2023-01-30 19:16:36 -0800

    Start by logging the currently selected node

--- src/ui/views/session/editor/EditorView.tsx
+++ src/ui/views/session/editor/EditorView.tsx
@@ -1,9 +1,36 @@
+import { useEffect } from "react";
+
 import "./EditorView.css";
 import { useEditor } from "../../../hooks/useEditor";
 import { HierarchyPanel } from "./HierarchyPanel";
+import { useMainThreadContext } from "../../../hooks/useMainThread";
+import { getModule } from "../../../../engine/module/module.common";
+import { EditorModule } from "../../../../engine/editor/editor.main";
+import { getLocalResource } from "../../../../engine/resource/resource.main";
 
 export function EditorView() {
   const { loading, scene, activeEntity, selectedEntities } = useEditor();
+
+  const mainThread = useMainThreadContext();
+
+  useEffect(() => {
+    let animationFrame: number;
+
+    function update() {
+      const editor = getModule(mainThread, EditorModule);
+
+      console.log(getLocalResource(mainThread, editor.activeEntity));
+
+      animationFrame = requestAnimationFrame(update);
+    }
+
+    update();
+
+    return () => {
+      cancelAnimationFrame(animationFrame);
+    };
+  }, [mainThread]);
+
   return (
     <>
       {loading || !scene ? null : (
