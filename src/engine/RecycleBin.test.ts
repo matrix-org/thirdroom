6082ec3f8005ee355dd394aa33de57d05bbfb4f0
Nate Martin <natem@element.io>
2023-01-31 11:29:56 -0800

    recycle bin test fixes

--- src/engine/RecycleBin.test.ts
+++ src/engine/RecycleBin.test.ts
@@ -2,7 +2,7 @@ import { createWorld } from "bitecs";
 import { describe, it, expect } from "vitest";
 
 import { World } from "./GameTypes";
-import { createRecycleBinContext, recycleBinNextTick, recycleBinReleaseEntities, recycleEntity } from "./RecycleBin";
+import { createRecycleBinContext, recycleBinNext, recycleBinRelease, addToRecycleBin } from "./RecycleBin";
 
 let disposed: number[] = [];
 
@@ -14,8 +14,8 @@ function addEntity(world: World): number {
   return id;
 }
 
-function removeEntity(world: World, eid: number) {
-  disposed.push(eid);
+function removeEntity(bin: number[]) {
+  disposed.push(...bin);
 }
 
 const resetGlobals = () => {
@@ -24,37 +24,37 @@ const resetGlobals = () => {
 };
 
 describe("RecycleBin", () => {
-  describe("recycleEntity", () => {
+  describe("recycle", () => {
     beforeEach(resetGlobals);
     it("should push an entity to the active recycle bin", () => {
       const world = createWorld() as World;
 
-      const recycleCtx = createRecycleBinContext();
+      const recycleCtx = createRecycleBinContext<number>();
 
       const eid = addEntity(world);
 
-      recycleEntity(recycleCtx, eid);
+      addToRecycleBin(recycleCtx, eid);
 
       expect(recycleCtx.active.length).toEqual(1);
       expect(recycleCtx.active[0]).toEqual(eid);
     });
   });
 
-  describe("recycleBinNextTick", () => {
+  describe("recycleBinNext", () => {
     beforeEach(resetGlobals);
     it("should swap the active recycle bin and add it to the historian", () => {
       const world = createWorld() as World;
 
-      const recycleCtx = createRecycleBinContext();
+      const recycleCtx = createRecycleBinContext<number>();
 
       const tick = 1;
       const eid1 = addEntity(world);
       const eid2 = addEntity(world);
 
-      recycleEntity(recycleCtx, eid1);
-      recycleEntity(recycleCtx, eid2);
+      addToRecycleBin(recycleCtx, eid1);
+      addToRecycleBin(recycleCtx, eid2);
 
-      recycleBinNextTick(recycleCtx, tick);
+      recycleBinNext(recycleCtx, tick);
 
       expect(recycleCtx.active.length).toEqual(0);
       expect(recycleCtx.historian.history.length).toEqual(1);
@@ -66,52 +66,52 @@ describe("RecycleBin", () => {
     });
   });
 
-  describe("recycleBinReleaseEntities", () => {
+  describe("recycleBinRelease", () => {
     beforeEach(resetGlobals);
     it("should call removeEntity on all entities that were in the recycle bins up to lastProcessedTick", () => {
       const world = createWorld({}, 10) as World;
 
-      const recycleCtx = createRecycleBinContext();
+      const recycleCtx = createRecycleBinContext<number>();
 
       // Tick 1 we create 1 entity but don't dispose it
       const tick1 = 1;
-      recycleBinNextTick(recycleCtx, tick1);
+      recycleBinNext(recycleCtx, tick1);
       const eid1 = addEntity(world);
-      recycleBinReleaseEntities(recycleCtx, world, tick1, removeEntity);
+      recycleBinRelease(recycleCtx, tick1, removeEntity);
       expect(disposed.length).toEqual(0);
 
       // end Tick 1
 
       // Tick 2 we dispose eid1 and eid3
       const tick2 = 2;
-      recycleBinNextTick(recycleCtx, tick2);
+      recycleBinNext(recycleCtx, tick2);
       addEntity(world); // eid2
       const eid3 = addEntity(world);
 
       expect(recycleCtx.active.length).toEqual(0);
-      recycleEntity(recycleCtx, eid1);
-      recycleEntity(recycleCtx, eid3);
+      addToRecycleBin(recycleCtx, eid1);
+      addToRecycleBin(recycleCtx, eid3);
       expect(recycleCtx.active.length).toEqual(2);
 
       expect(recycleCtx.pool.free.length).toEqual(0);
-      recycleBinReleaseEntities(recycleCtx, world, tick2, removeEntity);
+      recycleBinRelease(recycleCtx, tick2, removeEntity);
       expect(recycleCtx.pool.free.length).toEqual(1);
       // end Tick 2
 
       // Tick 3 we create eid4 and eid5 which should be recycled entities
       const tick3 = 3;
-      recycleBinNextTick(recycleCtx, tick3);
+      recycleBinNext(recycleCtx, tick3);
       // const eid4 = addEntity(world); // Should be the recycled eid1
       // const eid5 = addEntity(world); // Should be the recycled eid3
-      recycleBinReleaseEntities(recycleCtx, world, tick3, removeEntity);
+      recycleBinRelease(recycleCtx, tick3, removeEntity);
       expect(disposed.length).toEqual(2);
       // end Tick 3
 
       const tick4 = 4;
-      recycleBinNextTick(recycleCtx, tick4);
+      recycleBinNext(recycleCtx, tick4);
       const eid4 = addEntity(world); // Should be the recycled eid1
       const eid5 = addEntity(world); // Should be the recycled eid4
-      recycleBinReleaseEntities(recycleCtx, world, tick4, removeEntity);
+      recycleBinRelease(recycleCtx, tick4, removeEntity);
       // end Tick 4
 
       expect(recycleCtx.active.length).toEqual(0);
