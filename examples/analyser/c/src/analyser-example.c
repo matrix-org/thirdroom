8f32d26c3da8bed28d7f3ca6d8836d6f5e425fd3
Nate Martin <natem@element.io>
2023-02-03 16:08:14 -0800

    time data

--- examples/analyser/c/src/analyser-example.c
+++ examples/analyser/c/src/analyser-example.c
@@ -13,12 +13,13 @@ export void websg_deallocate(void *ptr) {
   free(ptr);
 }
 
-unsigned char *time_data[256] = {0};
 unsigned char *frequency_data[256] = {0};
+unsigned char *time_data[256] = {0};
 
 export void websg_loaded() {
 }
 
 export void websg_update(float_t dt) {
   thirdroom_get_audio_frequency_data(frequency_data);
+  thirdroom_get_audio_frequency_data(time_data);
 }
