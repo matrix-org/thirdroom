9770bddd5a6514d9f1ecb4b59aa0ef448b3ee2cc
Robert Long <robert@robertlong.me>
2023-03-08 11:14:38 -0800

    Add IPSME Module

--- src/engine/config.main.ts
+++ src/engine/config.main.ts
@@ -13,6 +13,7 @@ import {
   OutgoingMainThreadTripleBufferSystem,
 } from "./MainThreadTripleBufferSystems";
 import { MatrixModule } from "./matrix/matrix.main";
+import { IPSMEModule } from "./ipsme/ipsme.main";
 
 export default defineConfig<IMainThreadContext>({
   modules: [
@@ -25,6 +26,7 @@ export default defineConfig<IMainThreadContext>({
     RendererModule,
     ThirdroomModule,
     MatrixModule,
+    IPSMEModule,
   ],
   systems: [
     IncomingMainThreadTripleBufferSystem,
