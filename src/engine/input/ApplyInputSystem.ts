e77b1df2145adb9c8831aa819deed694d881554b
Nate Martin <natem@element.io>
2023-03-09 14:52:11 -0800

    type fixes

--- src/engine/input/ApplyInputSystem.ts
+++ src/engine/input/ApplyInputSystem.ts
@@ -9,7 +9,16 @@ import { InputModule } from "./input.game";
 import { checkBitflag } from "../utils/checkBitflag";
 import { Keys } from "./KeyCodes";
 
-const out: InputComponentState = { inputSourceId: 0, componentId: 0, button: 0, xAxis: 0, yAxis: 0, state: 0 };
+const out: InputComponentState = {
+  inputSourceId: 0,
+  componentId: 0,
+  button: 0,
+  xAxis: 0,
+  yAxis: 0,
+  zAxis: 0,
+  wAxis: 0,
+  state: 0,
+};
 
 export function ApplyInputSystem(ctx: GameState) {
   const input = getModule(ctx, InputModule);
