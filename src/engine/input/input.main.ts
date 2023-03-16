1dedc738128d8b38edac11c827ea95f30945d36c
Nate Martin <natem@element.io>
2023-03-16 15:27:52 -0700

    reset input state upon window blur / pointer lock exit / mouse leave

--- src/engine/input/input.main.ts
+++ src/engine/input/input.main.ts
@@ -40,7 +40,7 @@ export const InputModule = defineModule<IMainThreadContext, MainInputModule>({
     const camRigModule = getModule(ctx, CameraRigModule);
     const { canvas } = ctx;
 
-    const last: { [key: string]: boolean } = {};
+    const lastKeyMap: { [key: string]: boolean } = {};
 
     function enqueue(
       inputSourceId: number,
@@ -52,13 +52,41 @@ export const InputModule = defineModule<IMainThreadContext, MainInputModule>({
       wAxis: number,
       state: number
     ) {
-      if (document.pointerLockElement !== canvas && !camRigModule.orbiting) {
+      const orbiting = camRigModule.orbiting;
+      const pointerLocked = document.pointerLockElement !== null;
+      if (!pointerLocked && !orbiting) {
         return;
       }
 
       if (!enqueueInputRingBuffer(irb, inputSourceId, componentId, button, xAxis, yAxis, zAxis, wAxis, state)) {
         console.warn("input ring buffer full");
       }
+    }
+
+    function forceEnqueue(
+      inputSourceId: number,
+      componentId: number,
+      button: number,
+      xAxis: number,
+      yAxis: number,
+      zAxis: number,
+      wAxis: number,
+      state: number
+    ) {
+      if (!enqueueInputRingBuffer(irb, inputSourceId, componentId, button, xAxis, yAxis, zAxis, wAxis, state)) {
+        console.warn("input ring buffer full");
+      }
+    }
+
+    function resetAllInputs() {
+      for (const code in lastKeyMap) {
+        if (!lastKeyMap[code]) continue;
+        forceEnqueue(InputSourceId.Keyboard, InputComponentId.KeyboardButton, 0, 0, 0, 0, 0, codeToKeyCode(code));
+        lastKeyMap[code] = false;
+      }
+      forceEnqueue(InputSourceId.Mouse, InputComponentId.MouseMovement, 0, 0, 0, 0, 0, 0);
+      forceEnqueue(InputSourceId.Mouse, InputComponentId.MouseButtons, 0, 0, 0, 0, 0, 0);
+      forceEnqueue(InputSourceId.Mouse, InputComponentId.MouseScroll, 0, 0, 0, 0, 0, 0);
     }
 
     function onMouseDown({ buttons }: MouseEvent) {
@@ -70,14 +98,14 @@ export const InputModule = defineModule<IMainThreadContext, MainInputModule>({
     }
 
     function onKeyDown({ code }: KeyboardEvent) {
-      if (last[code]) return;
-      last[code] = true;
+      if (lastKeyMap[code]) return;
+      lastKeyMap[code] = true;
 
       enqueue(InputSourceId.Keyboard, InputComponentId.KeyboardButton, 1, 0, 0, 0, 0, codeToKeyCode(code));
     }
 
     function onKeyUp({ code }: KeyboardEvent) {
-      last[code] = false;
+      lastKeyMap[code] = false;
       enqueue(InputSourceId.Keyboard, InputComponentId.KeyboardButton, 0, 0, 0, 0, 0, codeToKeyCode(code));
     }
 
@@ -89,24 +117,44 @@ export const InputModule = defineModule<IMainThreadContext, MainInputModule>({
       enqueue(InputSourceId.Mouse, InputComponentId.MouseScroll, 0, deltaX, deltaY, 0, 0, 0);
     }
 
-    function onBlur() {}
+    function onBlur() {
+      resetAllInputs();
+    }
+
+    function onMouseLeave() {
+      resetAllInputs();
+    }
+
+    function onPointerLockChange() {
+      if (document.pointerLockElement !== canvas) {
+        resetAllInputs();
+      }
+    }
 
     canvas.addEventListener("mousedown", onMouseDown);
     canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", onMouseLeave);
+    canvas.addEventListener("wheel", onWheel);
+
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("keyup", onKeyUp);
     window.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("blur", onBlur);
-    canvas.addEventListener("wheel", onWheel);
+    window.addEventListener("blur", onBlur);
+
+    document.addEventListener("pointerlockchange", onPointerLockChange);
 
     return () => {
       canvas.removeEventListener("mousedown", onMouseDown);
       canvas.removeEventListener("mouseup", onMouseUp);
+      canvas.removeEventListener("mouseleave", onMouseLeave);
+      canvas.removeEventListener("wheel", onWheel);
+
       window.removeEventListener("keydown", onKeyDown);
       window.removeEventListener("keyup", onKeyUp);
       window.removeEventListener("mousemove", onMouseMove);
-      canvas.removeEventListener("blur", onBlur);
-      canvas.removeEventListener("wheel", onWheel);
+      window.removeEventListener("blur", onBlur);
+
+      document.removeEventListener("pointerlockchange", onPointerLockChange);
     };
   },
 });
