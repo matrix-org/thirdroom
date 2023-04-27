4f07ba824b239d3bf915c70fb7448b059e4bfe64
Robert Long <robert@robertlong.me>
2023-04-27 10:56:27 -0700

    convert mxc urls to https

--- src/ui/hooks/useUpdateWorldMembers.ts
+++ src/ui/hooks/useUpdateWorldMembers.ts
@@ -3,21 +3,27 @@ import { useEffect } from "react";
 
 import { Thread } from "../../engine/module/module.common";
 import { ThirdRoomMessageType, UpdateWorldMembersMessage, WorldMember } from "../../plugins/thirdroom/thirdroom.common";
+import { useHydrogen } from "./useHydrogen";
 import { useMainThreadContext } from "./useMainThread";
 import { useStateEvents } from "./useStateEvents";
 
 export function useUpdateWorldMembers(world?: Room) {
+  const { session } = useHydrogen(true);
   const worldMembersEvents = useStateEvents(world, "org.matrix.msc3815.world.member");
   const mainThread = useMainThreadContext();
 
   useEffect(() => {
     const worldMembers: Map<string, WorldMember> = new Map(
-      Array.from(worldMembersEvents).map(([userId, event]) => [userId, { avatarUrl: event.content.avatar_url }])
+      Array.from(worldMembersEvents).map(([userId, event]) => {
+        const avatarMxcUrl = event.content.avatar_url;
+        const avatarUrl = avatarMxcUrl ? session.mediaRepository.mxcUrl(avatarMxcUrl) : undefined;
+        return [userId, { avatarUrl }];
+      })
     );
 
     mainThread.sendMessage<UpdateWorldMembersMessage>(Thread.Game, {
       type: ThirdRoomMessageType.UpdateWorldMembers,
       worldMembers,
     });
-  }, [mainThread, worldMembersEvents]);
+  }, [mainThread, worldMembersEvents, session]);
 }
